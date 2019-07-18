7da91d21f920301fd8c4b7820473ad86a153497f
Jeremy Miller <jgmiller88@gmail.com>
2019-07-18 08:20:04 -0700

    chore: Fix lint findings

--- internal/configuration/configuration_private_test.go
+++ internal/configuration/configuration_private_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestCalculateDays(t *testing.T) {
-	weekday, _ := time.Parse(time.RFC3339, "2019-07-18T15:00:00Z")
-	monday, _ := time.Parse(time.RFC3339, "2019-07-15T15:00:00Z")
+	weekday, _ := time.Parse(time.RFC3339, "2019-07-18T15:00:00Z") //nolint:errcheck
+	monday, _ := time.Parse(time.RFC3339, "2019-07-15T15:00:00Z")  //nolint:errcheck
 	testCases := []struct {
 		name     string
 		time     time.Time
@@ -19,6 +19,7 @@ func TestCalculateDays(t *testing.T) {
 		{name: "Monday", time: monday, expected: 3},
 	}
 	for _, tc := range testCases {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			actual := calculateDays(tc.time)
 			assert.Equal(t, tc.expected, actual)
